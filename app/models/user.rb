acf3389c89bd7d41e0b7d88e133a64a35ff20c9d
Jack Linton <jacklinton@mac.com>
2017-02-27 00:57:43 +0000

    Created local environment variables to keep from missing off AWS.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
 					  styles: { medium: "300x300>", thumb: "100x100>" },
 					  default_url: "/images/:style/missing.png",
 					  :storage => :s3
-					  :s3_region: ENV["us-east-1"],
+					  :s3_region: ENV["AWS_REGION"],
 					  :s3_credentials => Proc.new{|a| a.instance.s3_credentials}
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 	
 	def s3_credentials
-		{:bucket => "railsblogbucket", :access_key_id => "AKIAI44BETQHEEFUWN4A", :secret_access_key => "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"}
+		{:bucket => "railsblogbucket", :access_key_id => ENV["AWS_ACCESS_KEY_ID"], :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]}
 	end
 end
